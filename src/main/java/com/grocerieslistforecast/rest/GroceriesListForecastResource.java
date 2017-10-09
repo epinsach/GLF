f693eb5a000387e32d333939479a931da0987967
Enric Pinsach <enric.pinsach@t-systems.com>
2017-10-09 14:02:01 +0200

    First commit

---
+++ src/main/java/com/grocerieslistforecast/rest/GroceriesListForecastResource.java
@@ -0,0 +1,18 @@
+package com.grocerieslistforecast.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by epinsach on 09/10/2017.
+ */
+@Path("/glf")
+public class GroceriesListForecastResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt(){
+        return "Got it";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: epinsach
+  Date: 09/10/2017
+  Time: 9:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Simple Jersey</title>
+</head>
+<body>
+<p><a href="glf">GLF resource</a></p>
+</body>
+
+</html>
